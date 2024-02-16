4af696746ab19e99338897b5e2c9873aeeea181e
Janet <jane824691@gmail.com>
2024-02-16 15:20:20 +0800

    開發中

---
+++ routes/product.js
@@ -0,0 +1,140 @@
+import express from "express";
+import db from "../utils/connect-mysql.js";
+import upload from "../utils/upload-imgs.js";
+import dayjs from "dayjs";
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+  const u = req.url.split("?")[0]; // 只要路徑
+  console.log({ u });
+  if (req.method === "GET" && u === "/") {
+    return next();
+  }
+  /*
+  if (!req.session.admin) {
+    return res.redirect("/login");
+  } */
+  next();
+});
+
+const getListData = async (req) => {
+  const perPage = 12; // 每頁幾筆
+  let page = +req.query.page || 1; // 用戶決定要看第幾頁
+  let keyword =
+    req.query.keyword && typeof req.query.keyword === "string"
+      ? req.query.keyword.trim()
+      : "";
+  let keyword_ = db.escape(`%${keyword}%`);
+
+  let qs = {}; // 用來把 query string 的設定傳給 template
+  // 起始的日期
+  let startDate = req.query.startDate ? req.query.startDate.trim() : "";
+  const startDateD = dayjs(startDate);
+  if (startDateD.isValid()) {
+    startDate = startDateD.format("YYYY-MM-DD");
+  } else {
+    startDate = "";
+  }
+
+  // 結束的日期
+  let endDate = req.query.endDate ? req.query.endDate.trim() : "";
+  const endDateD = dayjs(endDate);
+  if (endDateD.isValid()) {
+    endDate = endDateD.format("YYYY-MM-DD");
+  } else {
+    endDate = "";
+  }
+
+  // 查關鍵字對應api
+  let where = ` WHERE 1 `;
+  if (keyword) {
+    qs.keyword = keyword;
+    where += ` AND ( \`name\` LIKE ${keyword_} OR \`mobile\` LIKE ${keyword_} ) `;
+  }
+  if (startDate) {
+    qs.startDate = startDate;
+    where += ` AND birthday >= '${startDate}' `;
+  }
+  if (endDate) {
+    qs.endDate = endDate;
+    where += ` AND birthday <= '${endDate}' `;
+  }
+
+  let totalRows = 0;
+  let totalPages = 0;
+  let rows = [];
+
+  let output = {
+    success: false,
+    page,
+    perPage,
+    rows,
+    totalRows,
+    totalPages,
+    qs,
+    redirect: "",
+    info: "",
+  };
+
+  if (page < 1) {
+    output.redirect = `?page=1`;
+    output.info = `頁碼值小於 1`;
+    return output;
+  }
+
+  const t_sql = `SELECT COUNT(1) totalRows FROM product ${where}`;
+  [[{ totalRows }]] = await db.query(t_sql);
+  totalPages = Math.ceil(totalRows / perPage);
+  if (totalRows > 0) {
+    if (page > totalPages) {
+      output.redirect = `?page=${totalPages}`;
+      output.info = `頁碼值大於總頁數`;
+      return { ...output, totalRows, totalPages };
+    }
+
+    const sql = `SELECT * FROM product ${where} ORDER BY pid DESC 
+    LIMIT ${(page - 1) * perPage}, ${perPage}`;
+    [rows] = await db.query(sql);
+    output = { ...output, success: true, rows, totalRows, totalPages };
+  }
+
+  return output;
+};
+
+router.get("/", async (req, res) => {
+  res.locals.pageName = "ab-list";
+  res.locals.title = "列表 | " + res.locals.title;
+  const output = await getListData(req);
+  if (output.redirect) {
+    return res.redirect(output.redirect);
+  }
+
+  if (!req.session.admin) {
+    res.render("product/list", output);
+  } else {
+    res.render("pid/list", output);
+  }
+});
+
+router.get("/api", async (req, res) => {
+  res.json(await getListData(req));
+  /*
+  if(res.locals.jwt?.id){
+    return res.json(await getListData(req));
+  } else {
+    return res.json({success: false, error: "沒有授權, 不能取得資料"});
+  }
+  */
+});
+
+router.get("/one/:pid", async (req, res) => {
+  let pid = +req.params.pid || 1;
+  const [rows, fields] = await db.query(
+    `SELECT DISTINCT product.*, product_mutiple_img.photo_content_main, product_mutiple_img.photo_content_secondary, product_mutiple_img.photo_content FROM product LEFT JOIN product_mutiple_img ON product.pid = product_mutiple_img.pid WHERE product.pid = ${pid};`
+  );
+  if (rows.length) return res.json(rows[0]);
+  else return res.json({});
+});
+
+export default router;
