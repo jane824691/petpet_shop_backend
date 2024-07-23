640ec415dabb298dd95f42cb8c371dc11b275b21
Janet <jane824691@gmail.com>
2024-07-23 23:30:40 +0800

    try removing import mysql

--- utils/connect-mysql.js
+++ utils/connect-mysql.js
@@ -2,7 +2,8 @@ import mysql from "mysql2/promise";
 
 const {DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT} =process.env;
 console.log({DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT});
-const db =await mysql.createPool({ //node的await不用async的樣子11/16,10:17
+// const db =await mysql.createPool({ //node的await不用async的樣子11/16,10:17
+const db =await createPool({
 host: DB_HOST,
 user: DB_USER,
 password: DB_PASS,
