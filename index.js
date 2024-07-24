669d93418dd524da3198d31857b4ab90e4eeddf3
Janet <jane824691@gmail.com>
2024-07-24 11:24:51 +0800

    change back webport

--- index.js
+++ index.js
@@ -343,7 +343,7 @@ app.use((req, res) => {
   res.status(404).send(`<h1>你迷路了嗎</h1>`);
 });
 
-const port = process.env.port || 3306;
+const port = process.env.WEB_PORT || 30553;
 
 app.listen(port, () => {
   console.log(`express server: ${port}`);
