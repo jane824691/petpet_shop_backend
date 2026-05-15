7ad7d2bfd6ddfd65e00012e9ae0fa59a59f28c84
Janet <jane824691@gmail.com>
2026-05-15 14:37:33 +0800

    feat: add connect url to Zeabur redis

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -2,7 +2,7 @@
 import { createClient } from 'redis'
 
 export const redisClient = createClient({
-    url: 'redis://127.0.0.1:6379',
+    url: 'redis://127.0.0.1:6379' || process.env.REDIS_URL,
     // Node.js經過DNS解析成 IPv6會變 localhost → ::1（IPv6）
     // 故直接寫 127.0.0.1（IPv4）
 
@@ -22,7 +22,7 @@ redisClient.on('error', (err) => {
     console.error('Redis Error', err)
 })
 
-// await redisClient.connect()
+await redisClient.connect()
 // await redisClient.set('foo', 'bar');
 // const result = await client.get('foo');
 // console.log(result)  // >>> bar
